9ec8ac1285943f00fe635ce3ead5139267d5443e
Kader Bozkaya <87239102+kaderbozkaya@users.noreply.github.com>
2024-05-15 17:10:47 +0300

    ikinci commit

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Todo() {
+    const [deger,setDeger]=useState("")
+    const [todoDizi,setTodoDizi]=useState([])
   return (
     <>
-    <input type="text" className='border-2 p-2'/>
-    <button className='bg-blue-400 text-white text-lg m-3 p-2 rounded'>Ekle</button>
+    <h1 className='text-3xl text-blue-500'>TODO LIST</h1>
+    <input type="text"
+    placeholder='Todo ekle...'
+    value={deger}
+    onChange={e=>setDeger(e.target.value)}
+     className='border-2 p-2'/>
+    <button
+    className='bg-blue-400 text-white text-lg m-3 p-2 rounded'>Ekle</button>
 
     </>
   )
--- src/Components/Todos.jsx
+++ src/Components/Todos.jsx
@@ -4,12 +4,11 @@ export default function Todos() {
   return (
     <>
     <div>
-        <p>Lorem, ipsum dolor.
-
-        <button className='bg-green-300 p-2 m-2 rounded'>Güncelle</button>
-        <button className='bg-red-400 p-2 m-2 rounded'>Sil</button>
-        </p>
-
+      <ol>
+        <li>Lorem ipsum dolor sit amet. </li>
+        <li>Lorem ipsum dolor sit amet.</li>
+        <li>Lorem ipsum dolor sit amet.</li>
+      </ol>
        
     </div>
    
