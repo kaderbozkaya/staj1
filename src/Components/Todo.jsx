64c7ccbdd62229833cc65d80a53329ac43464e1d
Kader Bozkaya <87239102+kaderbozkaya@users.noreply.github.com>
2024-05-15 09:52:50 +0300

    First commit

---
+++ src/Components/Todo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Todo() {
+  return (
+    <>
+    <input type="text" className='border-2 p-2'/>
+    <button className='bg-blue-400 text-white text-lg m-3 p-2 rounded'>Ekle</button>
+
+    </>
+  )
+}
---
+++ src/Components/Todos.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Todos() {
+  return (
+    <>
+    <div>
+        <p>Lorem, ipsum dolor.
+
+        <button className='bg-green-300 p-2 m-2 rounded'>Güncelle</button>
+        <button className='bg-red-400 p-2 m-2 rounded'>Sil</button>
+        </p>
+
+       
+    </div>
+   
+    </>
+  )
+}
