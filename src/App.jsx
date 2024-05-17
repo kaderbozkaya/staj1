cc1aa28f3ec6bde2eaada0efa21b83903a69a039
Kader Bozkaya <87239102+kaderbozkaya@users.noreply.github.com>
2024-05-17 11:11:56 +0300

    Guncelleme kismi yapildi.

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,95 @@
-import React, { useState } from 'react';
-import { FaTrashAlt } from 'react-icons/fa';
-import { MdEdit, MdDone } from 'react-icons/md';
-import { v4 as uuidv4 } from 'uuid'; //id değerleri için npm uuid'den indirilen paket
-import './App.css';
+import React, { useState } from "react";
+import { FaTrashAlt } from "react-icons/fa";
+import { MdEdit, MdDone } from "react-icons/md";
+import { v4 as uuidv4 } from "uuid"; //id değerleri için npm uuid'den indirilen paket
+import "./App.css";
 
 function App() {
-    const [deger, setDeger] = useState('');
-    const [todoDizi, setTodoDizi] = useState([]);
+  const [deger, setDeger] = useState("");
+  const [todoDizi, setTodoDizi] = useState([]);
+  const [duzenlemeModu, setDuzenlemeModu] = useState(false); //düzenleme işlemi yaplıp yapılmadığını belirlemek için(ekle butonunu düzenleme veya ekle olarak değiştirmek için)
+  const [duzenlenenItem, setDuzenlenenItem] = useState(null); //düzenlenen todo'nun id'sini tutmak için
 
-    const ekle = () => {
-        if (deger.trim() !== '') { //eğer değer boşluk karakterlerinden arınmış bir şekildeyse ve boş bir değer değilse sıraya ekle değeri
-
-      //----------------EKLEME İÇİN GEREKLİ FONKSİYON---------------
-            const yeniEklenen = {
-                id: uuidv4(), 
-                item: deger
-            };
-            setTodoDizi([...todoDizi, yeniEklenen]);
-            setDeger(''); //değer eklendikten sonra inputun içini boşaltıyoruz
-        }
-    };
-    //---------------SİLME İÇİN GEREKLİ FONKSİYON----------------
-    const itemSil=(id)=> {
-      const yeniDizi=todoDizi.filter(item=>item.id !==id)  /*todoDizi silme işlemi için kullanılıyor, filter ile dizi filtreleniyor seçtiğim id'yi kontrol ediyor silmek için filtreliyor eşleşmeyen todo'lar yeniDiziye atanıyor kalan yani seçilen todoDizi'de kalıyor ve siliniyor */
-
-      setTodoDizi(yeniDizi) //güncel diziyi set ediyoruz
+  const ekle = () => {
+    if (deger.trim() !== "") {
+      //eğer değer boşluk karakterlerinden arınmış bir şekildeyse ve boş bir değer değilse sıraya ekle değeri
+      //düzenleme modu aktifse yani true ise
+      if (duzenlemeModu) {
+        setTodoDizi(
+          todoDizi.map((item) =>
+            item.id === duzenlenenItem ? { ...item, item: deger } : item
+          )
+        ); //todoDizi listesindeki todoları günceller düzenlenen todoyu yeni değerle değiştirir
+        setDuzenlemeModu(false); //işlem bitinde düzenleme modu false yapılır
+        setDuzenlenenItem(null);
+        //düzenleme modu false ise ekleme işlemi için ekle butonu aktiftir
+      } else {
+        const yeniEklenen = {
+          id: uuidv4(),
+          item: deger,
+        };
+        setTodoDizi([...todoDizi, yeniEklenen]);
+      }
+      setDeger(""); //değer eklendikten sonra inputun içini boşaltıyoruz
     }
+  };
+  //---------------SİLME İÇİN GEREKLİ FONKSİYON----------------
+  const itemSil = (id) => {
+    const yeniDizi = todoDizi.filter(
+      (item) => item.id !== id
+    ); /*todoDizi silme işlemi için kullanılıyor, filter ile dizi filtreleniyor seçtiğim id'yi kontrol ediyor silmek için filtreliyor eşleşmeyen todo'lar yeniDiziye atanıyor kalan yani seçilen todoDizi'de kalıyor ve siliniyor */
 
-  //-----------------DÜZENLEME İÇİN GEREKLİ FONKSİYON-----------
+    setTodoDizi(yeniDizi); //güncel diziyi set ediyoruz
+  };
 
-    return (
-        <>
-            <h1 className='text-3xl text-blue-500'>TODO LIST</h1>
-            <input
-                type='text'
-                placeholder='Todo ekle...'
-                value={deger}
-                onChange={e => setDeger(e.target.value)}
-                className='border-2 p-2 outline-none rounded'
-            />
-            <button
-                onClick={ekle}
-                className='bg-blue-400 text-white text-lg m-3 p-2 rounded'
+  const itemDuzenle = (id) => {
+    const duzenlenecekItem = todoDizi.find((item) => item.id === id); //düzenlenecek id'yi buluyoruz.
+    setDeger(duzenlenecekItem.item); //seçilen düzenlenecek todo inputta görüntülenir
+    setDuzenlemeModu(true);
+    setDuzenlenenItem(id);
+  };
+
+  return (
+    <>
+      <h1 className="text-3xl text-blue-500">TODO LIST</h1>
+      <input
+        type="text"
+        placeholder="Todo ekle..."
+        value={deger}
+        onChange={(e) => setDeger(e.target.value)}
+        className="border-2 p-2 outline-none rounded"
+      />
+      <button
+        onClick={ekle}
+        className="bg-blue-400 text-white text-lg m-3 p-2 rounded"
+      >
+        {duzenlemeModu ? "Güncelle" : "Ekle"}
+      </button>
+      <div>
+        <ul className="bg-blue-600 m-7 p-5 rounded break-words  ">
+          {todoDizi.map((item) => (
+            <li
+              key={item.id}
+              className="text-white flex items-center justify-center m-3 border-2 rounded"
             >
-                Ekle
-            </button>
-            <div>
-                <ul className='bg-blue-600 m-7 p-5 rounded break-words  '>
-                    {todoDizi.map((item) => (
-                        <li key={item.id} className='text-white flex items-center justify-center m-3 border-2 rounded' >
-                            <p className='break-all'>{item.item}</p>
-                            <span className='flex justify-between p-3'>
-                                <FaTrashAlt className='cursor-pointer'
-                                onClick={()=>itemSil(item.id)}/>
-                                <MdEdit className='cursor-pointer '/>
-                                <MdDone className='cursor-pointer '/>
-                            </span>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </>
-    );
+              <p className="break-all">{item.item}</p>
+              <span className="flex justify-between p-3">
+                <FaTrashAlt
+                  className="cursor-pointer"
+                  onClick={() => itemSil(item.id)}
+                />
+                <MdEdit
+                  className="cursor-pointer "
+                  onClick={() => itemDuzenle(item.id)}
+                />
+                <MdDone className="cursor-pointer " />
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default App;
