7f5026c2301d5fe29949623b94b2d5390f38d995
Kader Bozkaya <87239102+kaderbozkaya@users.noreply.github.com>
2024-05-20 15:17:13 +0300

    Responsive design added

--- src/App.jsx
+++ src/App.jsx
@@ -2,118 +2,130 @@ import React, { useState } from "react";
 import { FaTrashAlt } from "react-icons/fa";
 import { MdEdit } from "react-icons/md";
 import { v4 as uuidv4 } from "uuid"; //id değerleri için npm uuid'den indirilen paket
-import { ImSad } from "react-icons/im";
-import { HiOutlineEmojiHappy } from "react-icons/hi";
+import { FaRegSquare } from "react-icons/fa";
+import { FaRegSquareCheck } from "react-icons/fa6";
 
 import "./App.css";
 
 function App() {
-  const [deger, setDeger] = useState("");
-  const [todoDizi, setTodoDizi] = useState([]);
-  const [duzenlemeModu, setDuzenlemeModu] = useState(false); //düzenleme işlemi yaplıp yapılmadığını belirlemek için(ekle butonunu düzenleme veya ekle olarak değiştirmek için)
-  const [duzenlenenItem, setDuzenlenenItem] = useState(); //düzenlenen todo'nun id'sini tutmak için
+  const [value, setValue] = useState("");
+  const [todoArray, setTodoArray] = useState([]);
+  const [editMode, setEditMode] = useState(false); //düzenleme işlemi yaplıp yapılmadığını belirlemek için(add butonunu update veya add olarak değiştirmek için)
+  const [editItemId, setEditItemId] = useState(); //düzenlenen todo'nun id'sini tutmak için
 
-  const ekle = () => {
-    if (deger.trim() !== "") {
-      //eğer değer boşluk karakterlerinden arınmış bir şekildeyse ve boş bir değer değilse sıraya ekle değeri
-      //düzenleme modu aktifse yani true ise
-      if (duzenlemeModu) {
-        setTodoDizi(
-          todoDizi.map((item) =>
-            item.id === duzenlenenItem ? { ...item, item: deger } : item
+  const add = () => {
+    if (value.trim() !== "") {
+      //eğer değer boşluk karakterlerinden arınmış bir şekildeyse ve boş bir değer değilse sıraya add değeri
+      //düzenleme Mode Active ise yani true ise
+      if (editMode) {
+        setTodoArray(
+          todoArray.map((item) =>
+            item.id === editItemId ? { ...item, item: value } : item
           )
-        ); //todoDizi listesindeki todoları günceller düzenlenen todoyu yeni değerle değiştirir
-        setDuzenlemeModu(false); //işlem bitinde düzenleme modu false yapılır
-        setDuzenlenenItem();
-        //düzenleme modu false ise ekleme işlemi için ekle butonu aktiftir
+        ); //todoArray listesindeki todoları günceller düzenlenen todoyu yeni değerle değiştirir
+        setEditMode(false); //işlem bitinde düzenleme modu false yapılır
+        setEditItemId();
+        //düzenleme modu false ise ekleme işlemi için add butonu aktiftir
       } else {
-        const yeniEklenen = {
+        const newItem = {
           id: uuidv4(),
-          item: deger,
-          durum: "Aktif",
+          item: value,
+          status: "Active",
         };
-        setTodoDizi([...todoDizi, yeniEklenen]);
+        setTodoArray([...todoArray, newItem]);
       }
-      setDeger(""); //değer eklendikten sonra inputun içini boşaltıyoruz
+      setValue(""); //değer eklendikten sonra inputun içini boşaltıyoruz
     }
   };
-  //---------------SİLME İÇİN GEREKLİ FONKSİYON----------------
-  const itemSil = (id) => {
-    const yeniDizi = todoDizi.filter(
+  const handleKeyPress = (e) => {
+    //enter'a basınca listeye todo eklesin diye
+    if (e.key === "Enter") {
+      add();
+    }
+  };
+  //---------------SİLME İÇİN GEREKLİ FONKSİYONLAR----------------
+  const deleteItem = (id) => {
+    const newArr = todoArray.filter(
       (item) => item.id !== id
-    ); /*todoDizi silme işlemi için kullanılıyor, filter ile dizi filtreleniyor seçtiğim id'yi kontrol ediyor silmek için filtreliyor eşleşmeyen todo'lar yeniDiziye atanıyor kalan yani seçilen todoDizi'de kalıyor ve siliniyor */
+    ); /*todoArray silme işlemi için kullanılıyor, filter ile Array filtreleniyor seçtiğim id'yi kontrol ediyor silmek için filtreliyor eşleşmeyen todo'lar newArrye atanıyor kalan yani seçilen todoArray'de kalıyor ve siliniyor */
 
-    setTodoDizi(yeniDizi); //güncel diziyi set ediyoruz
+    setTodoArray(newArr); //güncel Arrayyi set ediyoruz
   };
 
-  const itemDuzenle = (id) => {
-    const duzenlenecekItem = todoDizi.find((item) => item.id === id); //düzenlenecek id'yi buluyoruz.
-    setDeger(duzenlenecekItem.item); //seçilen düzenlenecek todo inputta görüntülenir
-    setDuzenlemeModu(true);
-    setDuzenlenenItem(id);
+  //---------------EKLEME VE GÜNCELLEME İÇİN GEREKLİ FONKSİYONLAR---------------------
+  const editItem = (id) => {
+    const updateItem = todoArray.find((item) => item.id === id); //düzenlenecek id'yi buluyoruz.
+    setValue(updateItem.item); //seçilen düzenlenecek todo inputta görüntülenir
+    setEditMode(true);
+    setEditItemId(id);
   };
-  const itemDurum = (id) => {
-    let durum =
-      todoDizi.filter((item) => item.id === id)[0].durum === "Aktif" //seçtiğim todo dizinin 0. yani ilk elemanı aktifse tamamlandı olarak değitiriyorum değilse aktif olarak değiştiriyorum
-        ? "Tamamlandı"
-        : "Aktif";
-    setTodoDizi((item) =>
-      item.map((item) => (item.id === id ? { ...item, durum } : item))
+  const itemstatus = (id) => {
+    let status =
+      todoArray.filter((item) => item.id === id)[0].status === "Active" //seçtiğim todo Arraynin 0. yani ilk elemanı Active ise Resolved olarak değitiriyorum değilse Active olarak değiştiriyorum
+        ? "Resolved"
+        : "Active";
+    setTodoArray((item) =>
+      item.map((item) => (item.id === id ? { ...item, status } : item))
     );
   };
 
   return (
     <>
-      <h1 className="text-4xl text-white">TODO LIST</h1>
-      <input
-        type="text"
-        placeholder="Todo ekle..."
-        value={deger}
-        size={40}
-        onChange={(e) => setDeger(e.target.value)}
-        className="border-2 p-2 outline-none rounded"
-      />
-      <button
-        onClick={ekle}
-        className="bg-blue-400 text-white text-xl m-3 p-2 rounded"
-      >
-        {duzenlemeModu ? "Güncelle" : "Ekle"}
-      </button>
-      <div>
-        <ul className="m-7 p-5 rounded break-words ">
-          {todoDizi.map((item) => (
+      <h1 className="text-2xl md:text-4xl  text-white text-center m-3">
+        TODO LIST
+      </h1>
+      <div className="flex items-center justify-center">
+        <input
+          type="text"
+          placeholder="Add todo..."
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          onKeyDown={handleKeyPress}
+          className="border-2  p-2 outline-none rounded-2xl w-44 md:w-80 h-10 md:h-16 text-xl md:text-3xl text-gray-600"
+        />
+        <button
+          onClick={add}
+          className="bg-blue-400 text-white text-lg md:text-2xl m-3 p-2 rounded-2xl w-20 md:w-28 h-10 md:h-16 hover:bg-blue-500 "
+        >
+          {editMode ? "Update" : "Add"}
+        </button>
+      </div>
+      <div className="flex items-center justify-center">
+        <ul className="m-5 p-3 rounded break-words flex flex-col items-center justify-center ">
+          {todoArray.map((item) => (
             <li
               key={item.id}
-              className={`text-white flex items-center justify-between m-3 border-2 rounded ${
-                item.durum === "Tamamlandı" ? "bg-green-800" : ""
+              className={`text-white flex items-center justify-between m-3 border-2 rounded w-[300px] md:w-[700px] ${
+                item.status === "Resolved" ? "bg-green-800" : ""
               }`}
             >
               <p
-                className={`break-all m-3 ${
-                  item.durum === "Tamamlandı" ? "line-through" : ""
+                className={`break-all m-3 text-xs md:text-2xl ${
+                  item.status === "Resolved" ? "line-through" : ""
                 }`}
               >
                 {item.item}
               </p>
               <span className="flex justify-between p-3 m-3">
                 <FaTrashAlt
-                  className="cursor-pointer text-2xl "
-                  onClick={() => itemSil(item.id)}
+                  className="cursor-pointer text-2xl md:text-4xl"
+                  onClick={() => deleteItem(item.id)}
                 />
                 <MdEdit
-                  className="cursor-pointer text-2xl ml-1"
-                  onClick={() => itemDuzenle(item.id)}
+                  className="cursor-pointer text-2xl md:text-4xl ml-1"
+                  onClick={() => editItem(item.id)}
                 />
-                {item.durum === "Aktif" && (
-                  <ImSad
-                    className="cursor-pointer text-2xl ml-1"
-                    onClick={() => itemDurum(item.id)}
+
+                {item.status === "Active" && (
+                  <FaRegSquare
+                    className="cursor-pointer text-2xl md:text-4xl ml-1"
+                    onClick={() => itemstatus(item.id)}
                   />
                 )}
-                {item.durum === "Tamamlandı" && (
-                  <HiOutlineEmojiHappy
-                    className="cursor-pointer text-2xl ml-1"
-                    onClick={() => itemDurum(item.id)}
+                {item.status === "Resolved" && (
+                  <FaRegSquareCheck
+                    className="cursor-pointer text-2xl md:text-4xl ml-1"
+                    onClick={() => itemstatus(item.id)}
                   />
                 )}
               </span>
